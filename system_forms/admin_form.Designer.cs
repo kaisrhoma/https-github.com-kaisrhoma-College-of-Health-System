b822cd1631cdebc55f8817bacba778da7d39646d
kaisrhoma <kaisrhoma@gmail.com>
2025-07-24 19:09:16 +0200

    kais is update at 7/24/25 , 7:09

--- system_forms/admin_form.Designer.cs
+++ system_forms/admin_form.Designer.cs
@@ -59,19 +59,19 @@
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(888, 537);
+            this.panel1.Size = new System.Drawing.Size(1182, 653);
             this.panel1.TabIndex = 0;
             // 
             // panel4
             // 
             this.panel4.AutoScroll = true;
             this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel4.Location = new System.Drawing.Point(0, 55);
-            this.panel4.Margin = new System.Windows.Forms.Padding(2);
+            this.panel4.Location = new System.Drawing.Point(0, 68);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(682, 482);
+            this.panel4.Size = new System.Drawing.Size(907, 585);
             this.panel4.TabIndex = 2;
             this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
             // 
@@ -84,9 +84,9 @@
             this.panel3.Controls.Add(this.pictureBox1);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(682, 55);
+            this.panel3.Size = new System.Drawing.Size(907, 68);
             this.panel3.TabIndex = 1;
             this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
             // 
@@ -96,10 +96,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(109)))), ((int)(((byte)(148)))));
-            this.label2.Location = new System.Drawing.Point(316, 14);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(419, 17);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.Size = new System.Drawing.Size(0, 32);
             this.label2.TabIndex = 3;
             // 
             // label1
@@ -107,10 +106,9 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(292, 14);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(387, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.Size = new System.Drawing.Size(0, 32);
             this.label1.TabIndex = 2;
             // 
             // label13
@@ -118,10 +116,9 @@
             this.label13.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(550, 14);
-            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label13.Location = new System.Drawing.Point(731, 17);
             this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(92, 25);
+            this.label13.Size = new System.Drawing.Size(116, 32);
             this.label13.TabIndex = 1;
             this.label13.Text = "مدير النظام";
             // 
@@ -130,10 +127,10 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::college_of_health_sciences.Properties.Resources.user;
-            this.pictureBox1.Location = new System.Drawing.Point(642, 10);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(854, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox1.Size = new System.Drawing.Size(47, 43);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -151,19 +148,19 @@
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.pictureBox2);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(682, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Location = new System.Drawing.Point(907, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(206, 537);
+            this.panel2.Size = new System.Drawing.Size(275, 653);
             this.panel2.TabIndex = 0;
             // 
             // panel11
             // 
             this.panel11.BackColor = System.Drawing.Color.White;
-            this.panel11.Location = new System.Drawing.Point(24, 172);
-            this.panel11.Margin = new System.Windows.Forms.Padding(2);
+            this.panel11.Location = new System.Drawing.Point(32, 212);
+            this.panel11.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel11.Name = "panel11";
-            this.panel11.Size = new System.Drawing.Size(162, 3);
+            this.panel11.Size = new System.Drawing.Size(216, 4);
             this.panel11.TabIndex = 8;
             // 
             // button7
@@ -177,11 +174,11 @@
             this.button7.ForeColor = System.Drawing.Color.White;
             this.button7.Image = global::college_of_health_sciences.Properties.Resources.cancel_24;
             this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button7.Location = new System.Drawing.Point(0, 462);
+            this.button7.Location = new System.Drawing.Point(0, 565);
             this.button7.Margin = new System.Windows.Forms.Padding(0);
             this.button7.Name = "button7";
-            this.button7.Padding = new System.Windows.Forms.Padding(0, 0, 19, 0);
-            this.button7.Size = new System.Drawing.Size(206, 35);
+            this.button7.Padding = new System.Windows.Forms.Padding(0, 0, 25, 0);
+            this.button7.Size = new System.Drawing.Size(275, 43);
             this.button7.TabIndex = 7;
             this.button7.Text = "تسجيل الخروج";
             this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -200,11 +197,11 @@
             this.button6.ForeColor = System.Drawing.SystemColors.Control;
             this.button6.Image = global::college_of_health_sciences.Properties.Resources.Folder_24;
             this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button6.Location = new System.Drawing.Point(0, 391);
+            this.button6.Location = new System.Drawing.Point(0, 481);
             this.button6.Margin = new System.Windows.Forms.Padding(0);
             this.button6.Name = "button6";
-            this.button6.Padding = new System.Windows.Forms.Padding(0, 0, 19, 0);
-            this.button6.Size = new System.Drawing.Size(206, 35);
+            this.button6.Padding = new System.Windows.Forms.Padding(0, 0, 25, 0);
+            this.button6.Size = new System.Drawing.Size(275, 43);
             this.button6.TabIndex = 6;
             this.button6.Text = "تعديل الملف الشخصي";
             this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -222,11 +219,11 @@
             this.button5.ForeColor = System.Drawing.SystemColors.Control;
             this.button5.Image = global::college_of_health_sciences.Properties.Resources.Folder_24;
             this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button5.Location = new System.Drawing.Point(0, 356);
+            this.button5.Location = new System.Drawing.Point(0, 438);
             this.button5.Margin = new System.Windows.Forms.Padding(0);
             this.button5.Name = "button5";
-            this.button5.Padding = new System.Windows.Forms.Padding(0, 0, 19, 0);
-            this.button5.Size = new System.Drawing.Size(206, 35);
+            this.button5.Padding = new System.Windows.Forms.Padding(0, 0, 25, 0);
+            this.button5.Size = new System.Drawing.Size(275, 43);
             this.button5.TabIndex = 5;
             this.button5.Text = "تقارير احصائية";
             this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -245,11 +242,11 @@
             this.button4.ForeColor = System.Drawing.SystemColors.Control;
             this.button4.Image = global::college_of_health_sciences.Properties.Resources.Folder_24;
             this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button4.Location = new System.Drawing.Point(0, 321);
+            this.button4.Location = new System.Drawing.Point(0, 395);
             this.button4.Margin = new System.Windows.Forms.Padding(0);
             this.button4.Name = "button4";
-            this.button4.Padding = new System.Windows.Forms.Padding(0, 0, 19, 0);
-            this.button4.Size = new System.Drawing.Size(206, 35);
+            this.button4.Padding = new System.Windows.Forms.Padding(0, 0, 25, 0);
+            this.button4.Size = new System.Drawing.Size(275, 43);
             this.button4.TabIndex = 4;
             this.button4.Text = "النسخ الأحتياطي";
             this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -267,11 +264,11 @@
             this.button3.ForeColor = System.Drawing.SystemColors.Control;
             this.button3.Image = global::college_of_health_sciences.Properties.Resources.Folder_24;
             this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button3.Location = new System.Drawing.Point(0, 286);
+            this.button3.Location = new System.Drawing.Point(0, 352);
             this.button3.Margin = new System.Windows.Forms.Padding(0);
             this.button3.Name = "button3";
-            this.button3.Padding = new System.Windows.Forms.Padding(0, 0, 19, 0);
-            this.button3.Size = new System.Drawing.Size(206, 35);
+            this.button3.Padding = new System.Windows.Forms.Padding(0, 0, 25, 0);
+            this.button3.Size = new System.Drawing.Size(275, 43);
             this.button3.TabIndex = 3;
             this.button3.Text = "إدارة المواد";
             this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -289,11 +286,11 @@
             this.button2.ForeColor = System.Drawing.SystemColors.Control;
             this.button2.Image = global::college_of_health_sciences.Properties.Resources.Folder_24;
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button2.Location = new System.Drawing.Point(0, 251);
+            this.button2.Location = new System.Drawing.Point(0, 309);
             this.button2.Margin = new System.Windows.Forms.Padding(0);
             this.button2.Name = "button2";
-            this.button2.Padding = new System.Windows.Forms.Padding(0, 0, 19, 0);
-            this.button2.Size = new System.Drawing.Size(206, 35);
+            this.button2.Padding = new System.Windows.Forms.Padding(0, 0, 25, 0);
+            this.button2.Size = new System.Drawing.Size(275, 43);
             this.button2.TabIndex = 2;
             this.button2.Text = "إدارة المستخدمين";
             this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -312,11 +309,11 @@
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
             this.button1.Image = global::college_of_health_sciences.Properties.Resources.Folder_24;
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.Location = new System.Drawing.Point(0, 216);
+            this.button1.Location = new System.Drawing.Point(0, 266);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
-            this.button1.Padding = new System.Windows.Forms.Padding(0, 0, 19, 0);
-            this.button1.Size = new System.Drawing.Size(206, 35);
+            this.button1.Padding = new System.Windows.Forms.Padding(0, 0, 25, 0);
+            this.button1.Size = new System.Drawing.Size(275, 43);
             this.button1.TabIndex = 1;
             this.button1.Text = "الرئيسية";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -329,23 +326,23 @@
             this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::college_of_health_sciences.Properties.Resources.cohs_logo;
-            this.pictureBox2.Location = new System.Drawing.Point(39, 14);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Location = new System.Drawing.Point(52, 17);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(133, 146);
+            this.pictureBox2.Size = new System.Drawing.Size(177, 180);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 0;
             this.pictureBox2.TabStop = false;
             // 
             // admin_form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.ClientSize = new System.Drawing.Size(888, 537);
+            this.ClientSize = new System.Drawing.Size(1182, 653);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MinimumSize = new System.Drawing.Size(904, 576);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(1200, 700);
             this.Name = "admin_form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
---
+++ system_forms/registerar_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace college_of_health_sciences.system_forms
+{
+    public partial class registerar_form : Form
+    {
+        public registerar_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
