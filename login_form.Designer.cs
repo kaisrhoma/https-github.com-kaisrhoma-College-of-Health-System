b0e750aefbcba7b788c23b1ac7a7e9ab084994f5
kaisrhoma <kaisrhoma@gmail.com>
2025-07-24 18:10:06 +0200

    kais edit

--- login_form.Designer.cs
+++ login_form.Designer.cs
@@ -304,6 +304,7 @@
             this.button3.TabIndex = 1;
             this.button3.Text = "Registrar";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
